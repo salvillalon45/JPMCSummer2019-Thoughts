aa5ecc9a1072f22776332985004eb143e586830e
Salvador Villalon <salvav1@uci.edu>
2019-09-09 14:59:51 -0700

    Summer 2019: Created a new branch to save code we have

--- jpmc_thoughts/src/Components/Pictures/Pictures.js
+++ jpmc_thoughts/src/Components/Pictures/Pictures.js
@@ -6,13 +6,42 @@ import pic_3 from "./pic_3.jpg";
 import pic_5 from "./pic_5.jpeg";
 import pic_6 from "./pic_6.jpeg";
 import pic_7 from "./pic_7.jpeg";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
 
 export class Pictures extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.handlePictureClick = this.handlePictureClick.bind(this);
-  }
+    this.state = {
+     showModal: false,
+     show: false,
+   };
+   // this.handlePictureClick = this.handlePictureClick.bind(this);
+   // this.handleOpenModal = this.handleOpenModal.bind(this);
+   // this.handleCloseModal = this.handleCloseModal.bind(this);
+   this.handleShow = this.handleShow.bind(this);
+   this.handleClose = this.handleClose.bind(this);
+ }
+
+ handleOpenModal () {
+   this.setState({ showModal: true });
+ }
+
+ handleCloseModal () {
+   this.setState({ showModal: false });
+ }
+
+ handleShow(id) {
+   console.log(id);
+   // this.setState({ show : true});
+   this.setState({ show : id});
+ }
+
+ handleClose() {
+   // this.setState({show: false});
+   this.setState({ show : null});
+ }
 
   handlePictureClick(event) {
     console.log("Inside handlePictureClick");
@@ -43,39 +72,70 @@ export class Pictures extends React.Component {
     }
   }
 
+
+
   render() {
     return (
+
+
       <div className="Pictures" id="pictures">
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <div className="title">
                 <h2> Pictures </h2>
-                <p>Here are some pictures of my adventures in Chicago! (This was my first time in Chicago).</p>
+                <p>Here are some pictures of my adventures in Chicago!</p>
               </div>
             </div>
           </div>
 
           <div className="row">
-            <div className="column">
-              <img id="myImg" className="responsive" onClick={this.handlePictureClick} src={ pic_2 } alt="My intern team for Summer 2019" />
-            </div>
 
-            <div className="column">
-              <img id="myImg" className="responsive" onClick={this.handlePictureClick} src={ pic_5 } alt="These are the interns in the 30th floor" />
-            </div>
+            <Button variant="primary" onClick={this.handleShow("pic_2")}>
+              <img id="myImg" className="responsive" src={ pic_2 } alt="My intern team for Summer 2019" />
+            </Button>
 
-            <div id="myModal" className="modal">
-              <span className="close">&times;</span>
-              <img className="modal-content" id="img01"/ >
-              <div id="caption"></div>
-            </div>
+            <Modal show={this.state.show === "pic_2"} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>My intern team for Summer 2019</Modal.Title>
+              </Modal.Header>
+              <Modal.Body><img className="responsive" src={ pic_2 } alt="My intern team for Summer 2019" /></Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
 
-            <div id="myModal" className="modal">
-              <span className="close">&times;</span>
-              <img className="modal-content" id="img02"/ >
-              <div id="caption"></div>
-            </div>
+            // <div className="column">
+            //   <img id="myImg" className="responsive" onClick={this.handlePictureClick} src={ pic_2 } alt="My intern team for Summer 2019" />
+            // </div>
+
+            <Button variant="primary" onClick={this.handleShow("pic_5")}>
+              <img id="myImg" className="responsive" src={ pic_5 } alt="These are the interns in the 30th floor" />
+            </Button>
+
+            <Modal show={this.state.show === "pic_5"} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>These are the interns in the 30th floor</Modal.Title>
+              </Modal.Header>
+              <Modal.Body><img className="responsive" src={ pic_5 } alt="These are the interns in the 30th floor" /></Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+
+            // <div className="column">
+            //   <img id="myImg" className="responsive" onClick={this.handlePictureClick} src={ pic_5 } alt="These are the interns in the 30th floor" />
+            // </div>
+
+            // <div id="myModal" className="modal">
+            //   <span className="close">&times;</span>
+            //   <img className="modal-content" id="img01"/ >
+            //   <div id="caption"></div>
+            // </div>
           </div>
 
           <div className="row">
